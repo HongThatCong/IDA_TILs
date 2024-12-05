43224dee9ce69c312b43e2696391462f7d21ebd3
Hồng Thất Công <67528122+HongThatCong@users.noreply.github.com>
2024-12-05 09:49:33 +0700

    Add files via upload

---
+++ NETFXSDK/netfxsdk.h
@@ -0,0 +1,45 @@
+//
+// Microsoft .NET Framework SDK 4.8
+// Defines (-D): COBJMACROS and CINTERFACE for C language, not used with C++
+//
+
+#pragma once
+
+#ifndef NETFXSDK
+#define NETFXSDK
+
+// Include enums_netfxsdk first
+#include "enums_netfxsdk.h"
+
+// Include Standard Headers, cor.h first
+#include <cor.h>
+#include <alink.h>
+#include <clrdata.h>
+#include <cordebug.h>
+#include <CorError.h>
+#include <CorHdr.h>
+#include <corhlpr.h>
+#include <corprof.h>
+#include <corpub.h>
+#include <corsym.h>
+#include <fusion.h>
+#include <gchost.h>
+#include <ICeeFileGen.h>
+#include <isolation.h>
+#include <ivalidator.h>
+#include <ivehandler.h>
+#include <metahost.h>
+#include <mscoree.h>
+#include <openum.h>
+#include <StrongName.h>
+#include <tlbref.h>
+#include <VerError.h>
+
+// Include generated headers which were created from *.tlb
+#include <mscorlib.tlh>
+#include <System.tlh>
+#include <System.Drawing.tlh>
+#include <System.EnterpriseServices.tlh>
+#include <System.Windows.Forms.tlh>
+
+#endif  // NETFXSDK
---
+++ NETFXSDK/test_netfxsdk.cpp
@@ -0,0 +1,14 @@
+#include "netfxsdk.h"
+
+//#define TEST
+
+#ifdef TEST
+
+#include <stdio.h>
+
+int main()
+{
+    printf("0x%X\n", CLR_E_BIND_NI_DEP_IDENTITY_MISMATCH);
+}
+
+#endif
